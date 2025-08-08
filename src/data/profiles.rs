e6783c7c6d365ec447b8c766789c1afbb4964896
PopeRigby <poperigby@mailbox.org>
2025-08-07 20:52:35 -0700

    Get rid of ResolvedModEntry
    If I can just look mods up by ID it's kinda useless

--- src/data/profiles.rs
+++ src/data/profiles.rs
@@ -2,36 +2,10 @@ use derive_more::{AsRef, From};
 use getset::Getters;
 use native_db::{Key, ToKey};
 use serde::{Deserialize, Serialize};
-use tracing::warn;
 use uuid::Uuid;
 
-use crate::data::{games::Game, mods::Mod};
+use crate::data::mods::{Mod, ModId};
 
-// #[derive(Serialize, Deserialize, Debug, Clone, Getters)]
-// #[getset(get = "pub")]
-// pub struct ModEntry {
-//     uuid: Uuid,
-//     enabled: bool,
-//     notes: String,
-// }
-//
-// impl ModEntry {
-//     pub fn new(uuid: Uuid) -> Self {
-//         Self {
-//             uuid,
-//             enabled: true,
-//             notes: "".to_string(),
-//         }
-//     }
-// }
-//
-// #[derive(Debug, Clone, Getters)]
-// #[getset(get = "pub")]
-// pub struct ResolvedModEntry {
-//     mod_ref: Mod,
-//     entry: ModEntry,
-// }
-//
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, AsRef, From)]
 pub struct ProfileId(Uuid);
 
@@ -46,10 +20,28 @@ impl ToKey for ProfileId {
 
 #[derive(Serialize, Deserialize, Debug, Clone, Getters)]
 #[getset(get = "pub")]
+pub struct ModEntry {
+    mod_id: ModId,
+    enabled: bool,
+    notes: String,
+}
+
+impl ModEntry {
+    pub fn new(mod_id: ModId) -> Self {
+        Self {
+            mod_id,
+            enabled: true,
+            notes: "".to_string(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Getters)]
+#[getset(get = "pub")]
 pub struct Profile {
     id: ProfileId,
     name: String,
-    // mod_entries: Vec<ModEntry>,
+    mod_entries: Vec<ModEntry>,
 }
 
 impl Profile {
@@ -57,28 +49,11 @@ impl Profile {
         Self {
             id: Uuid::new_v4().into(),
             name: name.to_string(),
-            // mod_entries: Vec::new(),
+            mod_entries: Vec::new(),
         }
     }
 
-    // pub fn add_mod(&mut self, new_mod: Mod) {
-    //     self.mod_entries.push(ModEntry::new(new_mod.uuid()));
-    // }
-    //
-    // pub fn resolve_mod_entries(&self, game: &Game) -> Vec<ResolvedModEntry> {
-    //     self.mod_entries
-    //         .iter()
-    //         .filter_map(|entry| match game.mods().get(&entry.uuid) {
-    //             // TODO: Use Rc<RefCell<T>> to eliminate use of clone
-    //             Some(mod_ref) => Some(ResolvedModEntry {
-    //                 mod_ref: mod_ref.clone(),
-    //                 entry: entry.clone(),
-    //             }),
-    //             None => {
-    //                 warn!("Mod with UUID {} not found", entry.uuid);
-    //                 None
-    //             }
-    //         })
-    //         .collect()
-    // }
+    pub fn add_mod(&mut self, new_mod: Mod) {
+        self.mod_entries.push(ModEntry::new(new_mod.id()));
+    }
 }
