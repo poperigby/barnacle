3e01053fe0b9561b93ee3ec33fb39a4fae9d32be
PopeRigby <poperigby@mailbox.org>
2025-10-25 13:56:26 -0700

    Testing

--- barnacle-gui/src/main.rs
+++ barnacle-gui/src/main.rs
@@ -1,15 +1,56 @@
-use barnacle_lib::State;
 use barnacle_lib::{DeployKind, ProfileMod, State};
+use slint::{ModelRc, StandardListViewItem};
 
 slint::include_modules!();
 
-/// Run the GUI
-pub fn main() {
-    let app = App::new().unwrap();
+type TableRow = ModelRc<StandardListViewItem>;
+type TableModel = ModelRc<TableRow>;
 
-    let state = State::new().unwrap();
-    let current_profile = state.current_profile();
+/// Run the GUI
+#[tokio::main]
+pub async fn main() {
+    let app = App::new().unwrap();
+    let library_manager = LibraryManager::new().unwrap();
+
+    app.on_start_window(move || {
+        let library_manager = library_manager.as_weak();
+        library_manager.unwrap().show().unwrap();
+    });
+
+    let mut state = State::new().unwrap();
+
+    let game_id = state
+        .add_game("Skyrim", DeployKind::Gamebryo)
+        .await
+        .unwrap();
+    let profile_id = state.add_profile(game_id, "Enderal").await.unwrap();
+    state.set_current_profile(profile_id).await.unwrap();
+
+    let current_profile = state.current_profile().await.unwrap().unwrap();
     // Get mods from current profile and build model from them
+    let mods = state.mods(current_profile).await.unwrap();
+
+    app.global::<ModTableData>()
+        .set_model(build_table_model(&mods));
 
     app.run().unwrap();
+}
+
+fn build_table_model(profile_mods: &[ProfileMod]) -> TableModel {
+    let mut rows = Vec::new();
+
+    for profile_mod in profile_mods {
+        let row = [
+            StandardListViewItem::from(if profile_mod.entry().enabled() {
+                "✅"
+            } else {
+                "❌"
+            }),
+            StandardListViewItem::from(profile_mod.data().name()),
+        ];
+
+        rows.push(TableRow::from(row));
+    }
+
+    TableModel::from(rows.as_slice())
 }
