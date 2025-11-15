37a9a4bb177157052d7fd58f5854e4a94b592e5b
PopeRigby <poperigby@mailbox.org>
2025-11-14 21:43:08 -0800

    gui: setup basic ui

--- barnacle-gui/src/main.rs
+++ barnacle-gui/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{Element, Task, application, widget::column};
+
+fn main() -> iced::Result {
+    application("Barnacle", App::update, App::view).run_with(App::new)
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    ModList,
+    LibraryManager,
+}
+
+#[derive(Default, Debug)]
+enum Page {
+    #[default]
+    ModList,
+    LibraryManager,
+}
+
+#[derive(Default, Debug)]
+struct App {
+    page: Page,
+}
+
+impl App {
+    fn new() -> (Self, Task<Message>) {
+        (Self::default(), Task::none())
+    }
+
+    fn update(&mut self, _message: Message) {}
+
+    fn view(&self) -> Element<'_, Message> {
+        match self.page {
+            Page::ModList => column!["Mod list!",],
+            Page::LibraryManager => column!["Library manager!"],
+        }
+        .into()
+    }
 }
