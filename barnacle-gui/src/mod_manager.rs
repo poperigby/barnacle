48fef44051260de9385879ecb5890e15a3484503
PopeRigby <poperigby@mailbox.org>
2025-11-14 22:54:19 -0800

    gui: some basic stuff

--- barnacle-gui/src/mod_manager.rs
+++ barnacle-gui/src/mod_manager.rs
@@ -1,4 +1,8 @@
-use iced::{Element, widget::column};
+use barnacle_lib::GameId;
+use iced::{
+    Element,
+    widget::{button, column, combo_box, row},
+};
 
 use crate::Message;
 
@@ -7,15 +11,30 @@ pub enum ModManagerMessage {}
 
 #[derive(Debug)]
 pub struct ModManagerPage;
+// {
+// games: combo_box::State<GameId>,
+// current_game: GameId,
+// };
 
 impl ModManagerPage {
     pub fn new() -> Self {
-        Self
+        Self {}
     }
 
     pub fn update(&mut self, _message: ModManagerMessage) {}
 
     pub fn view(&self) -> Element<'_, Message> {
-        column!["Mod list!",].into()
+        column![
+            row![
+                "Game:",
+                button("Play"),
+                "Profile:",
+                button("Library Manager"),
+                button("Settings"),
+                button("Notifications")
+            ],
+            row![button("Install"), button("Deploy"), button("Tools")]
+        ]
+        .into()
     }
 }
